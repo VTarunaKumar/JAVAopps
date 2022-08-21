69dc4f9c4acebdf54b1cb535d45255b9aca76328
VTarunaKumar <vtarunakumarrao@gmail.com>
2022-08-21 12:44:04 +0530

    1st code

--- src/lecture1/Opps1.java
+++ src/lecture1/Opps1.java
@@ -1,30 +1,27 @@
 package lecture1;
 
-import java.util.Arrays;
-
 public class Opps1 {
     public static void main(String[] args) {
         Student siba = new Student("Sibaaaa", 11, 90.2f);
-//        System.out.println(siba.rollNo);
-//        System.out.println(siba.name);
-//        System.out.println(siba.marks);
-//        siba = new Student();
-//        siba.rollNo = 1;
-//        siba.marks = 99;
-//        siba.name = "Tarun Kumar";
-//        siba.Greeting();
-//        siba.changeName("Tarun");
-//        System.out.println(siba.rollNo);
-//        System.out.println(siba.name);
-//        System.out.println(siba.marks);
-//        Student tarun = new Student("Tarun Kumar Rao", 2, 90);
-//        System.out.println(tarun.rollNo);
-//        System.out.println(tarun.name);
-//        System.out.println(tarun.marks);
+        System.out.println(siba.rollNo);
+        System.out.println(siba.name);
+        System.out.println(siba.marks);
+        siba = new Student();
+        siba.rollNo = 1;
+        siba.marks = 99;
+        siba.name = "Tarun Kumar";
+        siba.Greeting();
+        siba.changeName("Tarun");
+        System.out.println(siba.rollNo);
+        System.out.println(siba.name);
+        System.out.println(siba.marks);
+        Student tarun = new Student("Tarun Kumar Rao", 2, 90);
+        System.out.println(tarun.rollNo);
+        System.out.println(tarun.name);
+        System.out.println(tarun.marks);
 
-//        Student random = new Student(siba);
-//        System.out.println(random.name);
-
+        Student random = new Student(siba);
+        System.out.println(random.name);
 
 
     }
